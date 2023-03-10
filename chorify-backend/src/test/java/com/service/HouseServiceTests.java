48eb4f7f3650d037e445c6fb0b345c15a955ba33
Chrisovalantis Vatos <chrisovalantis.vatos@mail.mcgill.ca>
2023-03-10 10:05:30 -0500

    Finished service tests for House

--- chorify-backend/src/test/java/com/service/HouseServiceTests.java
+++ chorify-backend/src/test/java/com/service/HouseServiceTests.java
@@ -1,8 +1,11 @@
 package com.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.times;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -148,6 +151,103 @@ public class HouseServiceTests {
         assertTrue(newHouse.getNumberOfMembers() == numberOfMembers);
     }
 
+    @Test
+    public void updateHouseWithValidNewHouseNameAndInvalidId() {
 
+        int id = 2;
+        String newHouseName = "New House Name";
 
+        Mockito.when(houseRepository.findById(id)).thenReturn(null);
+
+        ChorifyException exception = assertThrows(ChorifyException.class, () -> houseService.updateHouseName(id, newHouseName));
+        // Mockito.when(houseRepository.save(Mockito.any(House.class))).thenReturn(oldHouseNewName);
+
+        assertTrue(exception.getMessage().equals("House with ID of " + id + " does not exist."));
+        assertTrue(exception.getStatus() == HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void updateHouseWithInvalidNewHouseNameAndValidId() {
+
+        int id = 2;
+        String oldHouseName = "Old House Name";
+        String newHouseName = "New House Name";
+        int numberOfMembers = 6;
+
+        House oldHouse = new House(oldHouseName, numberOfMembers);
+        House housWithAlreadyExistingName = new House(newHouseName, numberOfMembers);
+
+        Mockito.when(houseRepository.findById(id)).thenReturn(oldHouse);
+        Mockito.when(houseRepository.findByHouseName(newHouseName)).thenReturn(housWithAlreadyExistingName);
+
+        ChorifyException exception = assertThrows(ChorifyException.class, () -> houseService.updateHouseName(id, newHouseName));
+
+        assertTrue(exception.getMessage().equals("House with name " + newHouseName + " already exists."));
+        assertTrue(exception.getStatus() == HttpStatus.BAD_REQUEST);
+    }
+
+    @Test 
+    public void testIncrementNumberOfHouseMembersWithValidHouseId() {
+        
+        int id = 2;
+        String houseName = "New House Name";
+        int numberOfMembers = 6;
+
+        House houseToUpdate = new House(houseName, numberOfMembers);
+        House updatedHouseToReturn = houseToUpdate;
+        updatedHouseToReturn.setNumberOfMembers(houseToUpdate.getNumberOfMembers());
+        updatedHouseToReturn.setId(id);
+
+        Mockito.when(houseRepository.findById(id)).thenReturn(houseToUpdate);
+        Mockito.when(houseRepository.save(Mockito.any(House.class))).thenReturn(updatedHouseToReturn);
+
+        House  updatedHouse = houseService.incrementNumberOfHouseMembers(id);
+
+        assertNotNull(updatedHouse);
+        assertTrue(updatedHouse.getId() == id);
+        assertTrue(updatedHouse.getHouseName().equals(houseName));
+        assertEquals(numberOfMembers + 1, updatedHouse.getNumberOfMembers());
+    }
+
+    @Test
+    public void testIncrementNumberOfHouseMembersWithInvalidId() {
+
+        int id = 2;
+
+        Mockito.when(houseRepository.findById(id)).thenReturn(null);
+
+        ChorifyException exception = assertThrows(ChorifyException.class, () -> houseService.incrementNumberOfHouseMembers(id));
+
+        assertTrue(exception.getMessage().equals("House with ID of " + id + " does not exist."));
+        assertTrue(exception.getStatus() == HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void testDeleteByValidId() {
+
+        int id = 2;
+        String houseName = "New House Name";
+        int numberOfMembers = 6;
+
+        House houseToDelete = new House(houseName, numberOfMembers);
+
+        Mockito.when(houseRepository.findById(id)).thenReturn(houseToDelete);
+
+        houseService.deleteHouse(id);
+
+        Mockito.verify(houseRepository, times(1)).deleteById(id);
+    }
+
+    @Test
+    public void testDeleteByInvalidId() {
+
+        int id = 2;
+
+        Mockito.when(houseRepository.findById(id)).thenReturn(null);
+
+        ChorifyException exception = assertThrows(ChorifyException.class, () -> houseService.deleteHouse(id));
+
+        assertTrue(exception.getMessage().equals("House with ID of " + id + " does not exist."));
+        assertTrue(exception.getStatus() == HttpStatus.NOT_FOUND);
+    }
 }
